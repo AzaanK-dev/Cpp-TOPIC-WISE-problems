5020efb3428746388fc37e203753794ad4336467
Azaan Mehtab Khan <azaanmehtabk@gmail.com>
2025-12-10 20:31:21 +0500

    Fix condition for divisibilty

--- 03. LOOPS/FizzBuzz.cpp
+++ 03. LOOPS/FizzBuzz.cpp
@@ -7,11 +7,11 @@ int main(){
      cout << "Enter n: ";
      cin >> n;
      while(i<=n){
-         if(i%2==0 && i%3==0){
+         if(i%3==0 && i%5==0){
              cout << "FizzBuzz" << endl;
-         }else if(i%2==0){
-             cout << "Fizz" << endl;
          }else if(i%3==0){
+             cout << "Fizz" << endl;
+         }else if(i%5==0){
              cout << "Buzz" << endl;
          }else{
              cout << i << endl;
